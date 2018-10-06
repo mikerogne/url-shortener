811011139c6ef8e4d751fe544a8fe913c852031b
Brad Kilshaw <brad@etylogic.com>
2018-10-06 10:23:21 -0700

    Adding url expiry and expired global scope

--- app/Url.php
+++ app/Url.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\ExpiredScope;
 
 class Url extends Model
 {
+
     protected $guarded = ['id'];
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ExpiredScope);
+    }
+
 }
