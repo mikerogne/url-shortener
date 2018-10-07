b06ab705d27e77918566207ba67c8045de8ec0f9
Brad Kilshaw <brad@etylogic.com>
2018-10-06 20:15:49 -0700

    comments

--- app/Url.php
+++ app/Url.php
@@ -21,10 +21,22 @@ class Url extends Model
         static::addGlobalScope(new ExpiredScope);
     }
 
-    public function getSlugAttribute() {
+    /**
+     * Converts the models ID to a base 36 string to shorten it
+     *
+     * @return string
+     */
+    public function getSlugAttribute() : string {
         return $this->id ? base_convert($this->id, 10, 36) : null;
     }
 
+    /**
+     * Static function to convert any string to a base 10 version of itself
+     * Used to convert the URL slug (domain.com/u/{slug}) to an integer
+     * which we can then pass into Url::find() to look up the full url
+     *
+     * @return int
+     */
     public static function convert_slug_to_id($slug) : int {
         return base_convert($slug, 36, 10);
     }
