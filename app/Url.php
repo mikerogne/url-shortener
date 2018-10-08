88527f06419f32a53706fb7f44379daa82517283
Mike's Wife <mike.infinitywebcreations@gmail.com>
2018-10-08 17:58:29 -0500

    Add $url->compiled_url model accessor

--- app/Url.php
+++ app/Url.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Url extends Model
 {
     protected $guarded = ['id'];
+    protected $appends = ['compiled_url'];
+
+    public function getCompiledUrlAttribute()
+    {
+        return url('/' . $this->short_url);
+    }
 }
